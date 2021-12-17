7f60735bf00f8da1d5012d057b8f8dee2500b069
Esko Luontola <esko@luontola.fi>
2021-12-17 14:58:25 +0200

    Add S3 bucket

--- upload-bucket/lib/upload-bucket-stack.ts
+++ upload-bucket/lib/upload-bucket-stack.ts
@@ -1,16 +1,14 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from 'aws-cdk-lib';
+import { aws_s3 as s3 } from 'aws-cdk-lib';
 
-export class UploadBucketStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class UploadBucketStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'UploadBucketQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new s3.Bucket(this, 'UploadBucket', {
+      bucketName: "tdd-mooc-exercises",
+      versioned: true,
+      enforceSSL: true,
+    });
   }
 }
