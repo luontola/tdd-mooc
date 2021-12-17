5b11d965487d55696a77978befe0629d35ec3d2c
Esko Luontola <esko@luontola.fi>
2021-12-17 16:19:23 +0200

    Code reformat

--- upload-bucket/bin/upload-bucket.ts
+++ upload-bucket/bin/upload-bucket.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { UploadBucketStack } from '../lib/upload-bucket-stack';
+import "source-map-support/register"
+import * as cdk from "aws-cdk-lib"
+import { UploadBucketStack } from "../lib/upload-bucket-stack"
 
-const app = new cdk.App();
-new UploadBucketStack(app, 'UploadBucketStack', {
-  env: { account: '344906152169', region: 'eu-north-1' },
-});
+const app = new cdk.App()
+new UploadBucketStack(app, "UploadBucketStack", {
+  env: { account: "344906152169", region: "eu-north-1" },
+})
--- upload-bucket/lib/upload-bucket-stack.ts
+++ upload-bucket/lib/upload-bucket-stack.ts
@@ -1,14 +1,14 @@
-import * as cdk from 'aws-cdk-lib';
-import { aws_s3 as s3 } from 'aws-cdk-lib';
+import * as cdk from "aws-cdk-lib"
+import { aws_s3 as s3 } from "aws-cdk-lib"
 
 export class UploadBucketStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
-    new s3.Bucket(this, 'UploadBucket', {
+    new s3.Bucket(this, "UploadBucket", {
       bucketName: "tdd-mooc-exercises",
       versioned: true,
       enforceSSL: true,
-    });
+    })
   }
 }
