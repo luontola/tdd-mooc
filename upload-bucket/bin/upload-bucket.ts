d9042ade5c180087d04cda77a92c5a362ae2aaba
Esko Luontola <esko@luontola.fi>
2021-12-17 15:50:06 +0200

    Cleanup

--- upload-bucket/bin/upload-bucket.ts
+++ upload-bucket/bin/upload-bucket.ts
@@ -5,17 +5,5 @@ import { UploadBucketStack } from '../lib/upload-bucket-stack';
 
 const app = new cdk.App();
 new UploadBucketStack(app, 'UploadBucketStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
   env: { account: '344906152169', region: 'eu-north-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
