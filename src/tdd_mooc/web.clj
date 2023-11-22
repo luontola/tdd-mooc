e3a00212b22d39a4cbff92f098a17d19607e24a0
Esko Luontola <esko@luontola.fi>
2023-11-22 23:17:05 +0200

    Page layout using Hiccup

--- src/tdd_mooc/web.clj
+++ src/tdd_mooc/web.clj
@@ -1,10 +1,26 @@
 (ns tdd-mooc.web
-  (:require [stasis.core :as stasis]))
+  (:require [hiccup.page :as page]
+            [hiccup2.core :as h]
+            [stasis.core :as stasis]))
 
 (def export-dir "target/dist")
 
+(defn layout-page [content]
+  (str (h/html {:mode :html}
+               (page/doctype :html5)
+               [:html
+                [:head
+                 [:meta {:charset "utf-8"}]
+                 [:title "TDD MOOC"]
+                 [:meta {:name "viewport", :content "width=device-width, initial-scale=1.0"}]
+                 [:link {:rel "stylesheet", :href "/style.css"}]]
+                [:body
+                 [:h1 "TDD MOOC"]
+                 [:main content]]])))
+
 (defn get-pages []
-  (stasis/slurp-directory "resources/public" #".*\.(html|css|js)$"))
+  (merge (stasis/slurp-directory "resources/public" #".*\.(html|css|js)$")
+         {"/" (layout-page (h/raw "hello <u>there</u>"))}))
 
 (def app (stasis/serve-pages get-pages))
 
