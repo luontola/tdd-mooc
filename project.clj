952263e99b6953d7c5f6b8d474c7026cf1e79e3b
Esko Luontola <esko@luontola.fi>
2025-01-16 11:48:30 +0200

    Upgrade Clojure dependencies

--- project.clj
+++ project.clj
@@ -1,16 +1,16 @@
 (defproject tdd-mooc "1-SNAPSHOT"
 
-  :dependencies [[clj-commons/clj-yaml "1.0.27"]
+  :dependencies [[clj-commons/clj-yaml "1.0.29"]
                  [com.vladsch.flexmark/flexmark-all "0.64.8"]
                  [enlive "1.1.6"]
                  [hiccup "2.0.0-RC2"]
                  [medley "1.4.0"]
-                 [optimus "2023.11.21"]
-                 [org.clojure/clojure "1.11.1"]
-                 [org.clojure/java.data "1.0.95"]
-                 [ring/ring-core "1.11.0-RC1"]
+                 [optimus "2025.01.15"]
+                 [org.clojure/clojure "1.12.0"]
+                 [org.clojure/java.data "1.2.107"]
+                 [ring/ring-core "1.13.0"]
                  [stasis "2023.11.21"]]
-  :managed-dependencies []
+  :managed-dependencies [[org.jsoup/jsoup "1.18.3"]]
   :pedantic? :warn
 
   :source-paths ["src"]
@@ -26,7 +26,7 @@
   :aliases {"export" ["run" "-m" "tdd-mooc.web/export"]
             "kaocha" ["with-profile" "+kaocha" "run" "-m" "kaocha.runner"]}
 
-  :profiles {:dev {:dependencies [[lambdaisland/kaocha "1.87.1366"]
+  :profiles {:dev {:dependencies [[lambdaisland/kaocha "1.91.1392"]
                                   [org.clojure/test.check "1.1.1"]]
                    :plugins [[lein-ancient "0.7.0"]
                              [lein-ring "0.12.6"]]}
